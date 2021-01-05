5d68a296e0ca6ab9b5970d89c5568f659b0ee041
marlon-lopez <marlonlopez5647@gmail.com>
2021-01-05 15:17:15 -0600

    security dependencies added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config({ path: './config/config.env' })
-const mongoose = require('mongoose')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const hpp = require('hpp')
 const connecDB = require('./config/db')
 const { errorHandler, notFound } = require('./middleware/error')
 const cors = require('cors')
@@ -17,6 +21,19 @@ const app = express()
 //body parser
 app.use(express.json())
 
+//add sanitize to prevent SQL injection sanitize data
+app.use(mongoSanitize())
+
+//set security headers
+app.use(helmet())
+
+//prevent XSS attacts
+app.use(xss())
+
+//prevent htpp params pollution
+app.use(hpp())
+
+//enable CORS
 app.use(cors())
 
 app.use('/api/v1/auth', auth)
